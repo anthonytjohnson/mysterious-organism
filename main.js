f8b0b004542453d694d7a695008296d5eea88f54
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-10-05 18:14:15 +0900

    created factory function to create specimens

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 // Returns a random DNA base
 const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
-  return dnaBases[Math.floor(Math.random() * 4)];
-};
+  const dnaBases = ['A', 'T', 'C', 'G']
+  return dnaBases[Math.floor(Math.random() * 4)] 
+}
 
 // Returns a random single stand of DNA containing 15 bases
 const mockUpStrand = () => {
-  const newStrand = [];
+  const newStrand = []
   for (let i = 0; i < 15; i++) {
-    newStrand.push(returnRandBase());
+    newStrand.push(returnRandBase())
   }
-  return newStrand;
-};
+  return newStrand
+}
+
+const pAequorFactory = (number, array) => {
+  return {
+    specimenNum: number,
+    dna: array
+  }
+}
 
 
 
